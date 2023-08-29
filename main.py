d0ed0955265c3dbcc6ccf274a617bf9b0d61054e
Antonio Carlos de Lima Junior <ac@marketmining.com.br>
2023-08-29 02:44:10 -0300

    setup csv

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main(cfg : DictConfig):
     dengue = build_pipeline_dengue(pipeline, cfg.datasets.dengue)
     chuvas = build_pipeline_chuvas(pipeline, cfg.datasets.chuvas)
     dataset_chuvas_dengue = build_pipeline_join_chuvas_dengue(chuvas, dengue)
-    save(dataset_chuvas_dengue)
+    save(dataset_chuvas_dengue, cfg.datasets.output)
 
     # run pipeline
     pipeline.run()
--- src/save.py
+++ src/save.py
@@ -6,12 +6,15 @@ def prepare_csv(element, separator=";"):
     return f"{separator}".join(element)
 
 
-def save(dataset):
+def save(dataset, output_setup):
     tag = "[save]/"
     result = (
         (dataset)
-        | f"{tag}Prepare CSV" >> beam.Map(prepare_csv)
-        | f"{tag}Write to Text" >> WriteToText("./data/processed/output.csv")
+        | f"{tag}Prepare CSV" >> beam.Map(prepare_csv, output_setup.separator)
+        | f"{tag}Write to Text" >> 
+            WriteToText(file_path_prefix=output_setup.path, 
+                        file_name_suffix=output_setup.suffix,
+                        header=output_setup.header)
         # | f"{tag}Print" >> beam.Map(print)        
     )
     return result
