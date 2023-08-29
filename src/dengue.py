3d08bd095a537ce33cea94ab79be590473be1585
Antonio Carlos de Lima Junior <ac@marketmining.com.br>
2023-08-29 00:11:38 -0300

    pipeline dengue

--- src/dengue.py
+++ src/dengue.py
@@ -1,4 +1,5 @@
 import apache_beam as beam
+import re
 from apache_beam.io import ReadFromText
 from .utils import list_to_dict
 
@@ -7,9 +8,19 @@ def add_field_ano_mes(element):
     element["ano_mes"] = "-".join(element["data_iniSE"].split("-")[:2])
     return element
 
-def add_key(element):
+
+def add_key_uf(element):
     return (element["uf"], element)
 
+
+def add_key_ano_mes(element):
+    uf, data = element
+    for item in data:
+        if bool(re.search(r'\d', item["casos"])) == True:
+          yield (f"{uf}-{item['ano_mes']}", float(item["casos"]))
+        else:
+          yield (f"{uf}-{item['ano_mes']}", 0.0)
+    
 
 def build_pipeline(pipeline, dataset_setup):
 
@@ -21,7 +32,9 @@ def build_pipeline(pipeline, dataset_setup):
         | "Text to list" >> beam.Map(lambda line: line.split(dataset_setup.separator))
         | "List to dict" >> beam.Map(list_to_dict, dataset_setup.columns)
         | "Add 'ano_mes'" >> beam.Map(add_field_ano_mes)
-        | "Add key" >> beam.Map(add_key)
-        | "Group by key" >> beam.GroupByKey()
+        | "Add key uf" >> beam.Map(add_key_uf)
+        | "Group by key uf" >> beam.GroupByKey()
+        | "Add key uf-ano-mes" >> beam.FlatMap(add_key_ano_mes)
+        | "Reduce (sum casos)" >> beam.CombinePerKey(sum)
         | "Show results" >> beam.Map(print)
     )
