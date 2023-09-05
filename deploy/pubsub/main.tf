a6e032e0be259f2506b9d0b9ba361def642ae751
Antonio Carlos de Lima Junior <ac@marketmining.com.br>
2023-09-05 12:01:01 -0300

    setup mde-topic

--- deploy/pubsub/main.tf
+++ deploy/pubsub/main.tf
@@ -1,10 +1,44 @@
-resource "google_pubsub_topic" "petrobras_topic" {
-    name     = "petrobras-topic"
+resource "google_pubsub_topic" "demo_topic" {
+    name     = "demo-topic"
 }
 
-resource "google_pubsub_subscription" "petrobras1-subscription" {
-  name  = "petrobras1-subscription"
-  topic = google_pubsub_topic.petrobras_topic.name
+resource "google_pubsub_subscription" "demo_subscription" {
+  name  = "demo-subscription"
+  topic = google_pubsub_topic.demo_topic.name
+
+  # 20 minutes
+  message_retention_duration = "1200s"
+  retain_acked_messages      = true
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering    = false
+}
+
+resource "google_pubsub_topic" "mde_topic" {
+    name     = "mde-topic"
+}
+
+locals {
+  subscriber_list = [
+      {name: "cloudstorage-ingestion"}, 
+      {name: "bigquery-ingestion"}, 
+      {name: "bigtable-ingestion"}      
+  ]
+}
+
+resource "google_pubsub_subscription" "subscriptions" {
+  for_each = { for obj in local.subscriber_list : "${obj.name}" => obj }
+  name     = "${each.value.name}/" # Declarin
+
+  topic = google_pubsub_topic.mde_topic.name
 
   # 20 minutes
   message_retention_duration = "1200s"
